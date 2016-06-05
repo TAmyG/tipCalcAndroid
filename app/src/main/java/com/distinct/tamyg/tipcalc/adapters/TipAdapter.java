d4026147ec439b11117aad4c16561ae3913d54f9
Your Name <tamy.vivas@gmail.com>
2016-06-05 14:30:03 -0600

    commit final

--- app/src/main/java/com/distinct/tamyg/tipcalc/adapters/TipAdapter.java
+++ app/src/main/java/com/distinct/tamyg/tipcalc/adapters/TipAdapter.java
@@ -2,7 +2,6 @@ package com.distinct.tamyg.tipcalc.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +10,9 @@ import android.widget.TextView;
 import com.distinct.tamyg.tipcalc.R;
 import com.distinct.tamyg.tipcalc.models.TipRecord;
 
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadPoolExecutor;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
